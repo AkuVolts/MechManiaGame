b37ef94e6290eb45ac46f85e63b3a54d9e5806e9
Siddhant Kale <sskale3@illinois.edu>
2022-10-01 00:40:39 -0500

    Correcter Item Stats

--- MechManiaGame/Python-Starterpack/game/item.py
+++ MechManiaGame/Python-Starterpack/game/item.py
@@ -8,13 +8,30 @@ class ItemModel():
         self.cost = cost
 
 class Item(Enum):
-    PROCRUSTEAN_IRON = ItemModel(StatSet(0, 0, 0, 0), 1, 8)
-    HEAVY_BROADSWORD = ItemModel(StatSet(0, 0, 0, 0), -1, 8)
-    MAGIC_STAFF = ItemModel(StatSet(0, 0, 0, 0), 0, 8)
-    STEEL_TIPPED_ARROW = ItemModel(StatSet(0, 0, 0, 0), 0, 8)
-    ANEMOI_WINGS = ItemModel(StatSet(0, 0, 2, 0), -1, 10)
+
+    #Misc
+    SHIELD = ItemModel(StatSet(0,0,0,0), 1,8)
+
+    #Damage Items
+    PROCRUSTEAN_IRON = ItemModel(StatSet(0, 0, 0, 0), -1, 8)
     STRENGTH_POTION = ItemModel(StatSet(0, 4, 0, 0), 1, 5)
+    RALLY_BANNER = ItemModel(StatSet(0,2,0,0), -1, 8)
+
+    #Speed Items
+    ANEMOI_WINGS = ItemModel(StatSet(0, 0, 1, 0), -1, 8)
     SPEED_POTION = ItemModel(StatSet(0, 0, 2, 0), 1, 5)
+
+    #Range Items
     DEXTERITY_POTION = ItemModel(StatSet(0, 0, 0, 2), 1, 5)
+    HUNTING_SCOPE = ItemModel(StatSet(0,0,0,1), -1, 8)
+
+    #Chenge Items
+    HEAVY_BROADSWORD = ItemModel(StatSet(0, 0, 0, 0), 0, 10) #Change to knight
+    MAGIC_STAFF = ItemModel(StatSet(0, 0, 0, 0), 0, 10) #Change to Wizard
+    STEEL_TIPPED_ARROW = ItemModel(StatSet(0, 0, 0, 0), 0, 10) #Change to archer
+    
+    
+    
+    
     NONE = ItemModel(StatSet(0, 0, 0, 0), -1, 100)
 
